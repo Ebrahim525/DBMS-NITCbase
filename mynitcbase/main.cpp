382597158eeda20267b1356d71ade54f7d7e8271
Ebrahim525 <69968365+Ebrahim525@users.noreply.github.com>
2025-02-16 01:25:00 +0530

    Stage1

--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -28,88 +28,9 @@ void ex1() {
     printf("%u ", buffer3[i]);
   std::cout << "\n";
 }
-
-void stage2() {
-  RecBuffer relCatBuffer(RELCAT_BLOCK);
-  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
-
-  HeadInfo relCatHeader;
-  HeadInfo attrCatHeader;
-
-  relCatBuffer.getHeader(&relCatHeader);
-  attrCatBuffer.getHeader(&attrCatHeader);
-
-
-
-  
-  //Exercise 1
-
-  for(int i=0; i<attrCatHeader.numEntries; i++)
-  {
-    Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
-    attrCatBuffer.getRecord(attrCatRecord, i);
-    if(strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, "Students")==0 && strcmp(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, "RollNumber") == 0){
-      strcpy(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, "RegNO");
-      attrCatBuffer.setRecord(attrCatRecord, i);
-      break;
-    }
-  }
-
-
- for(int i=0; i<relCatHeader.numEntries; i++){
-
-  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
-  attrCatBuffer.getHeader(&attrCatHeader);
-
-  Attribute relCatRecord[RELCAT_NO_ATTRS];
-  relCatBuffer.getRecord(relCatRecord, i);
-
-  printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
-
-  for(int j=0; j<attrCatHeader.numEntries; j++){
-    
-    Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
-    attrCatBuffer.getRecord(attrCatRecord, j);
-    if(strcmp(relCatRecord[RELCAT_REL_NAME_INDEX].sVal, attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal)==0){
-        const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
-        printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
-    }
-
-   if(j==attrCatHeader.numSlots-1){
-      j=-1;
-      attrCatBuffer = RecBuffer(attrCatHeader.rblock);
-      attrCatBuffer.getHeader(&attrCatHeader);
-    }
-  }
- }
-}
-
-void stage3() {
-  for (int i=0; i<3; i++) {
-    RelCatEntry relCatEntry;
-    RelCacheTable::getRelCatEntry(i, &relCatEntry);
-    printf("Relation: %s\n", relCatEntry.relName);
-    for(int j=0; j<relCatEntry.numAttrs; j++){
-      AttrCatEntry attrCatEntry;
-      AttrCacheTable::getAttrCatEntry(i, j, &attrCatEntry);
-      char attrType[4];
-
-      if(attrCatEntry.attrType==0){
-        strcpy(attrType, "NUM");
-      }
-      else{
-        strcpy(attrType, "STR");
-      }
-      printf("  %s: %s\n", attrCatEntry.attrName, attrType);
-       
-    }
-  }
-}
-
 int main(int argc, char *argv[]) {
   Disk disk_run;
-  StaticBuffer buffer;
-  OpenRelTable cache;
-
-  return FrontendInterface::handleFrontend(argc, argv);
+  stage1();
+  ex1();
+  return 0;
 }
